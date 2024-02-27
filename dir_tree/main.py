f6f01e6e42b978b98cd78baac7c81017b3bf9966
DRH <dragonfourtyseven@gmail.com>
2024-02-27 05:57:39 +0500

    v1.5

--- dir_tree/main.py
+++ dir_tree/main.py
@@ -1,14 +1,58 @@
-"""
----------------------------
-   Writer: dragon-devs
-   Github: https://github.com/dragon-devs
----------------------------
-"""
 import os
 import argparse
 
-from . import generate_tree
-from . import save_tree_to_file
+
+def generate_tree_str(folder_path, indent='', include_hidden=False):
+   tree_str = ''
+   num_dirs = 0
+   num_files = 0
+
+   try:
+      items = os.listdir(folder_path)
+   except PermissionError:
+      print(f"Permission denied: {folder_path}")
+      return tree_str, num_dirs, num_files
+
+   for i, item in enumerate(items):
+      if not include_hidden and item.startswith('.'):
+         continue
+
+      if i == len(items) - 1:
+         branch = '└── '
+         new_indent = indent + '    '
+      else:
+         branch = '├── '
+         new_indent = indent + '│   '
+      item_path = os.path.join(folder_path, item)
+
+      if os.path.isdir(item_path):
+         item_display = f"📂 {item}"
+         num_dirs += 1
+      else:
+         item_display = f"📄 {item}"  # File icon
+         num_files += 1
+      tree_str += f"{indent}{branch}{item_display}\n"
+
+      if os.path.isdir(item_path):
+         subtree, dirs, files = generate_tree_str(item_path, new_indent, include_hidden)
+         tree_str += subtree
+         num_dirs += dirs
+         num_files += files
+
+   return tree_str, num_dirs, num_files
+
+
+def save_project_structure_to_file(file_path, include_hidden=False):
+   current_directory = os.getcwd()
+   tree, num_dirs, num_files = generate_tree_str(current_directory, include_hidden=include_hidden)
+
+   with open(file_path, 'w', encoding='utf-8') as file:
+      file.write(f"Project structure for directory: {current_directory}\n\n")
+      file.write(tree)
+      file.write(f"\n\nTotal directories 📂: {num_dirs}\n")
+      file.write(f"Total files 📄: {num_files}\n")
+
+   print(f"Project structure saved to {file_path}")
 
 def main():
    parser = argparse.ArgumentParser(description='Display directory tree structure')
@@ -17,13 +61,13 @@ def main():
    args = parser.parse_args()
 
    if args.output:
-      save_tree_to_file.save_project_structure_to_file(args.output, include_hidden=args.hidden)
+      save_project_structure_to_file(args.output, include_hidden=args.hidden)
    else:
       current_directory = os.getcwd()
-      tree, num_dirs, num_files = generate_tree.generate_tree_str(current_directory, include_hidden=args.hidden)
+      tree, num_dirs, num_files = generate_tree_str(current_directory, include_hidden=args.hidden)
       print(tree)
-      print(f"Total directories: {num_dirs}")
-      print(f"Total files: {num_files}")
+      print(f"Total directories 📂: {num_dirs}")
+      print(f"Total files 📄: {num_files}")
       print(f"\nIf you liked it give us a star & follow for more :) ... https://github.com/dragon-devs/dir-tree-drh")
 
 
--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dir-tree-drh',
-    version='1.4',
-    packages=find_packages(),
-    description="Creating a directory tree is an efficient method for visually organizing your project's directory structure, complete with detailed information. It provides a clear model of your project layout for easy reference and management.",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='dragon-devs',
-    author_email='dragonfourtyseven@email.com',
-    url='https://github.com/dragon-devs/dir-tree-drh',
-    license='MIT',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    entry_points={
-        'console_scripts': [
-            'dir-tree = dir_tree.main:main'
-        ]
-    }
+   name='dir-tree-drh',
+   version='1.5',
+   packages=find_packages(),
+   description="Creating a directory tree is an efficient method for visually organizing your project's directory structure, complete with detailed information. It provides a clear model of your project layout for easy reference and management.",
+   long_description=open('README.md', encoding='utf-8').read(),
+   long_description_content_type='text/markdown',
+   author='dragon-devs',
+   author_email='dragonfourtyseven@email.com',
+   url='https://github.com/dragon-devs/dir-tree-drh',
+   license='MIT',
+   classifiers=[
+      'Programming Language :: Python :: 3',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+   ],
+   entry_points={
+      'console_scripts': [
+         'dir-tree = dir_tree.main:main'
+      ]
+   }
 )
